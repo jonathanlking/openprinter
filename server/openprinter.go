a3174d4ea59d8ec07675f90ca2033f1ce51c2bd0
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-06-20 17:53:27 +0100

    Create a basic Hello, World! landing page

--- server/openprinter.go
+++ server/openprinter.go
@@ -15,10 +15,17 @@ func main() {
 
 	http.HandleFunc("/ping", ping)
 
+	http.HandleFunc("/", hello)
+
 	log.Printf("Listening on Port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "pong")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// TODO: Remove `client/dist` as a static file wrapper
+	http.ServeFile(w, r, "client/dist/index.html")
 }
